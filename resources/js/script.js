863d7b784ddf5e8b75f8ca9bec94cc6d51bfced0
lockdownlol <lockdownlol@users.noreply.github.com>
2020-05-31 14:07:04 +0100

    v2MVP

---
+++ resources/js/script.js
@@ -0,0 +1,20 @@
+new Twitch.Embed("twitch-embed", {
+    width: "100%",
+    height: "100%",
+    channel: "sinatraa",
+    layout: "video",
+    autoplay: false,
+});
+
+// $Lightning.use("c:ldlol_lightningout_app", function () {
+//     $Lightning.createComponent("c:ldlol_signup", {},
+//         "lightning",
+//         function (cmp) {
+//             console.log("LWC component was created");
+//             // do some stuff
+//         }
+//     );
+// },
+//     "https://lockdownlol-developer-edition.um1.force.com/"
+// );
+
